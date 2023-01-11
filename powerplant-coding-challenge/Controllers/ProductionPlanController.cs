be226b16fd4f86e91d0c193a560a3e0c1f20e157
Ioannis <ioannis.karapostolis@hstucia.fr>
2023-01-11 15:38:53 +0100

    Add Controller for production plan calculation

---
+++ powerplant-coding-challenge/Controllers/ProductionPlanController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace powerplant_coding_challenge.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductionPlanController : ControllerBase
+    {
+        [HttpPost(Name = "productionplan")]
+        public IActionResult Get()
+        {
+            return Ok("Endpoint working");
+        }
+    }
+}
