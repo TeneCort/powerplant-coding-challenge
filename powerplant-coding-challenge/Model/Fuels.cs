a57fc3974dad6743ef04bf6a24f31cfc98ea551c
Ioannis <ioannis.karapostolis@hstucia.fr>
2023-01-11 16:09:50 +0100

    Add Fuels model

---
+++ powerplant-coding-challenge/Model/Fuels.cs
@@ -0,0 +1,10 @@
+﻿namespace powerplant_coding_challenge.Model
+{
+    public class Fuels
+    {
+        public decimal Gas { get; set; }
+        public decimal Kerosine { get; set;}
+        public decimal Co2 { get; set;}
+        public decimal Wind { get; set;}
+    }
+}
