ae27b4dc8f30f56a701e919ef75cba50c48286cc
Ioannis <ioannis.karapostolis@hstucia.fr>
2023-01-11 16:09:59 +0100

    Add Payload model

---
+++ powerplant-coding-challenge/Model/Payload.cs
@@ -0,0 +1,9 @@
+﻿namespace powerplant_coding_challenge.Model
+{
+    public class Payload
+    {
+        public int Load { get; set; }
+        public Fuels Fuels { get; set; }
+        public List<Powerplant> Powerplants { get; set; } 
+    }
+}
