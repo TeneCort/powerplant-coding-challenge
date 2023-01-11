6a23b7130184f4b886eb35bfbfb35b6b9f5e4c52
Ioannis <ioannis.karapostolis@hstucia.fr>
2023-01-11 15:59:18 +0100

    Add Powerplant model class

---
+++ powerplant-coding-challenge/Model/Powerplant.cs
@@ -0,0 +1,14 @@
+﻿namespace powerplant_coding_challenge.Model
+{
+    public class Powerplant
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        //TODO change to enum
+        public decimal Efficiency { get; set; }
+        public int Pmin { get; set; }
+        public int Pmax { get; set; }
+
+        // TODO Cost calculation
+    }
+}
