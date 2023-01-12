6dbc454a742b423e716ee09e6f493dbb40b1975d
Ioannis <ioannis.karapostolis@hstucia.fr>
2023-01-12 09:38:06 +0100

    Add power plant DTO class

---
+++ powerplant-coding-challenge/DTO/PowerplantDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace powerplant_coding_challenge.DTO
+{
+    public class PowerplantDTO
+    {
+        public PowerplantDTO(string name, double generatedPower) 
+        {
+            Name = name;
+            GeneratedPower = generatedPower;
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("p")]
+        public double GeneratedPower { get; set; }
+    }
+}
