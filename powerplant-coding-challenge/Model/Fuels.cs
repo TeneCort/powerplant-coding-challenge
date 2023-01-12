348350d6f9d46704d9d2b328487fb387516bd16b
Ioannis <ioannis.karapostolis@hstucia.fr>
2023-01-12 08:59:21 +0100

    Modify Fuels class for serialization mapping

--- powerplant-coding-challenge/Model/Fuels.cs
+++ powerplant-coding-challenge/Model/Fuels.cs
@@ -1,14 +1,23 @@
-﻿namespace powerplant_coding_challenge.Model
+﻿using System.Text.Json.Serialization;
+
+namespace powerplant_coding_challenge.Model
 {
     public class Fuels
     {
-        public decimal Gas { get; set; }
-        public decimal Kerosine { get; set;}
-        public decimal Co2 { get; set;}
-        public decimal Wind { get; set;}
+
+        [JsonConstructor]
+        public Fuels() { }
+
+        [JsonPropertyName("gas(euro/MWh)")]
         public double Gas { get; set; }
+
+        [JsonPropertyName("kerosine(euro/MWh)")]
         public double Kerosine { get; set;}
+
+        [JsonPropertyName("co2(euro/ton)")]
         public double Co2 { get; set;}
+
+        [JsonPropertyName("wind(%)")]
         public double Wind { get; set;}
     }
 }
